040bd4d1afcdd6f06042d77be66e9e374496a310
Mike Maxwell <madmax98087@yahoo.com>
2015-11-11 18:08:46 -0800

    timmer device

---
+++ devicetypes/mikemaxwell/momentary-timer-button.src/momentary-timer-button.groovy
@@ -0,0 +1,61 @@
+/*
+	Timmer button
+*/
+metadata {
+	definition (name: "Momentary Timer Button", namespace: "MikeMaxwell", author: "Mike Maxwell") {
+		capability "Switch"
+		capability "Momentary"
+        capability "Switch Level"
+	}
+
+	// simulator metadata
+	simulator {
+	}
+    preferences {
+       	input( 
+        	name		: "delay"
+        	,type		: "enum"
+            ,title		: "Delay time:"
+            ,required	: true
+            ,options	: [["30":"30 Seconds"],["60":"1 Minute"],["120":"2 Minutes"],["300":"5 Minutes"],["600":"10 Minutes"]]
+        )
+    }
+
+	// UI tile definitions
+	tiles {
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: 'Start', action: "momentary.push", backgroundColor: "#ffffff" //, nextState: "running"
+			state "on", label: 'Running', action: "momentary.push", backgroundColor: "#fe7f00"
+		}
+		main "switch"
+		details "switch"
+	}
+}
+
+def parse(String description) {
+}
+
+def push() {
+    def delay = settings.delay.toInteger() ?: 10
+    runDelay(delay)
+}
+
+def on() {
+    log.info "on..."
+	push()
+}
+
+def off() {
+	log.info "off..."
+	sendEvent(name: "switch", value: "off")
+}
+def setLevel(delayMinutes){
+	def delay = delayMinutes * 60
+    runDelay(delay)
+}
+def runDelay(seconds){
+	log.info "running - delay:${seconds}"
+	sendEvent(name: "switch", value: "on")
+    sendEvent(name: "momentary", value: "pushed", isStateChange: true)
+	runIn(seconds,off)
+}
